0da378919517e9e95d841215665a8386db7ed5ff
Tigran-fd <tigran.hovhannisyan.fd@gmail.com>
2025-02-10 17:10:07 +0400

    fix code after review

--- main.py
+++ main.py
@@ -1,118 +1,117 @@
 import csv
 import datetime
 
-def file_reader(filename) -> list:
+def read_file(filename: str) -> list:
     with open(filename) as file:
         tsv_file = csv.reader(file, delimiter="\t")
         return list(tsv_file)
 
-def getting_today_timestamp() -> float:
+def get_timestamp() -> str:
     current_time = datetime.datetime.now()
-    current_timestamp = current_time.timestamp()
-    return current_timestamp
+    return str(current_time.timestamp())
 
-def filtering_data_columns(datalist: list,
-                           *columns: str) -> list:
+def filter_columns(datalist: list,
+                   columns: list) -> list:
+
     header = datalist[1]
-
     column_indexes = [header.index(col) for col in columns]
 
-    filtered_data = [list(columns)]
+    filtered_data = []
+
     for row in datalist[2:]:
         try:
             filtered_row = [row[index] for index in column_indexes]
             filtered_data.append(filtered_row)
         except IndexError:
-            raise ValueError(f"Row {row} is missing required columns.") from None
+            raise IndexError(f"Row {row} is missing required columns.") from None
 
     return filtered_data
 
-def filtiring_stars_by_fov(datalist: list,
-                           ra: float,
-                           dec: float,
-                           fov_h: float,
-                           fov_v: float,
-                           ):
-    fov_h_max, fov_h_min = (ra+fov_h/2)%360,(ra-fov_h/2)%360
-    fov_v_max, fov_v_min = dec+fov_v/2, ra-fov_v/2
-    if fov_v_max > 90:
-        raise ValueError(f"fov_h must be less than 90")
-    elif fov_v_min < -90:
-        raise ValueError(f"fov_h must be less than -90")
+def filter_stars_by_fov(datalist: list,
+                        ra: float,
+                        dec: float,
+                        fov_h: float,
+                        fov_v: float) -> list:
+    fov_h_max, fov_h_min = (ra + fov_h / 2) % 360, (ra - fov_h / 2) % 360
+    fov_v_max, fov_v_min = dec + fov_v / 2, dec - fov_v / 2
 
-    for row in datalist[1:]:
+    if fov_v_max > 90 or fov_v_min < -90:
+        raise ValueError("FOV should be in the range [-90, 90].")
+
+    filtered_data = []
+
+    for row in datalist:
         try:
-            star_ra = float(row[0])
-            star_dec = float(row[1])
-            if not(fov_h_min <= star_ra <= 360 and
-                            0 <= star_ra <= fov_h_max and
-                     fov_v_min <= star_dec <= fov_v_max):
-                datalist.remove(row)
+            star_ra = float(row[1])
+            star_dec = float(row[2])
+            if fov_h_min <= star_ra <= fov_h_max and fov_v_min <= star_dec <= fov_v_max:
+                filtered_data.append(row)
         except ValueError:
             continue
-    return
 
-#Insertion sort algorithm
-def sorting_brightest_N_stars(datalist: list,
-                              number_of_sturs: int
-                              ):
-    for i in range(2, len(datalist)):
+    return filtered_data
 
-        a = datalist[i]
-        j = i - 1
-        while j >= 1 and a[3] < datalist[j][3]:
-            datalist[j + 1] = datalist[j]
-            j -= 1
+def sort_brightest_n_stars(datalist: list,
+                           number_of_stars: int) -> list:
+    sorted_data = []
 
-        datalist[j + 1] = a
-    for row in datalist[number_of_sturs+1:]:
-        datalist.remove(row)
-    return
+    for current_star in datalist:
+        try:
+            brightness = float(current_star[3])
+        except ValueError:
+            sorted_data.append(current_star)
+            continue
+        position = len(sorted_data) - 1
+        while position >= 0 and float(sorted_data[position][3]) < brightness:
+            position -= 1
 
-def find_distance(first_ra: float,
-                  first_dec: float,
-                  second_ra: float,
-                  second_dec: float,
-                  ) -> float:
-    distance = ((first_ra-second_ra)**2 + (first_dec-second_dec)**2)**0.5
-    return distance
+        sorted_data.insert(position + 1, current_star)
+
+    return sorted_data[:number_of_stars]
+
+def calculate_distance(ra_1: float,
+                       dec_1: float,
+                       ra_2: float,
+                       dec_2: float) -> float:
+    return ((ra_1 - ra_2) ** 2 + (dec_1 - dec_2) ** 2) ** 0.5
 
 def add_distance_columns(datalist: list,
                          ra: float,
-                         dec: float,
-                         ):
-    datalist[0].append('distance')
-    for row in datalist[1:]:
-        row.append(find_distance(ra, dec, float(row[1]), float(row[2])))
-    return
+                         dec: float) -> list:
+    for row in datalist:
+        row.append(calculate_distance(ra, dec, float(row[1]), float(row[2])))
+    return datalist
 
-def create_csv(stars_data: list,):
-    csv_file_path = "{0}.csv".format(str(getting_today_timestamp()))
+def create_csv(stars_data: list,
+               header: list) -> None:
+    csv_file_path = f"{get_timestamp()}.csv"
     with open(csv_file_path, mode='w', newline='') as file:
         writer = csv.writer(file)
+        writer.writerow(header)
         writer.writerows(stars_data)
 
 def main():
-    print(-0.3 % 360)
     ra = float(input("Input RA: "))
     dec = float(input("Input DEC: "))
     fov_h = float(input("Input FOV_H: "))
     fov_v = float(input("Input FOV_V: "))
     number_of_stars = int(input("Input number_of_stars: "))
 
-    tcv_file = file_reader("cleaned_stars.tsv")
+    tsv_file = read_file("cleaned_stars.tsv")
 
-    needed_columns = ("source_id", "ra_ep2000", "dec_ep2000", "b")
+    needed_columns = ["source_id", "ra_ep2000", "dec_ep2000", "b"]
 
-    stars_data = filtering_data_columns(tcv_file, *needed_columns)
+    stars_data = filter_columns(tsv_file, needed_columns)
 
-    filtiring_stars_by_fov(stars_data, ra, dec, fov_h, fov_v,)
+    filtered_data = filter_stars_by_fov(stars_data, ra, dec, fov_h, fov_v)
 
-    sorting_brightest_N_stars(stars_data, number_of_stars)
+    sorted_data = sort_brightest_n_stars(filtered_data, number_of_stars)
 
-    add_distance_columns(stars_data,ra,dec)
+    data_with_distance_columns = add_distance_columns(sorted_data, ra, dec)
 
-    create_csv(stars_data)
+    csv_file_columns = needed_columns + ["distance"]
+
+    create_csv(data_with_distance_columns, csv_file_columns)
 
 if __name__ == "__main__":
     main()
